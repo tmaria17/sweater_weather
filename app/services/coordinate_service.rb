d8acc09fc61e6113f1b03ba63171f1fa65b8dc8d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-04 15:10:39 -0700

    Creates a service to get coordinates.

--- app/services/coordinate_service.rb
+++ app/services/coordinate_service.rb
@@ -4,17 +4,27 @@ class CoordinateService
     @location = location
   end
 
+  def get_coordinates
+    get_location[0][:geometry][:location]
+  end
+
+  def get_location
+    get_json("/maps/api/geocode/json?address=#{@location}")
+  end
 
 
 
   private
 
     def conn
-      Faraday.new(url:"https://maps.googleapis.com/maps/api/geocode/") do |faraday|
-        faraday.headers["Accept"] = "application/json"
-        faraday.headers["key"] = ENV['google_key']
+      Faraday.new(url:"https://maps.googleapis.com") do |faraday|
+        faraday.params["key"] = ENV['google_key']
         faraday.adapter Faraday.default_adapter
       end
+    end
+
+    def get_json(url)
+      JSON.parse(conn.get(url).body, symbolize_names: true)[:results]
     end
 
 end
