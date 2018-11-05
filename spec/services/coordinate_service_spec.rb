13b74b7307111da904eee7670719b099c6b5789d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-04 21:48:43 -0700

    Add webmock to existing tests.

--- spec/services/coordinate_service_spec.rb
+++ spec/services/coordinate_service_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 describe CoordinateService do
+  before :each do
+    json_response = File.open("./fixtures/coordinate_response.json")
+    stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=10000 E Alameda Avenue,  Denver,  CO&key=#{ENV['google_key']}")
+    .to_return(status: 200, body: json_response)
+    @coordinates_service = CoordinateService.new('Denver, CO')
+
+  end
   it 'exists' do
-    coordinate_service = CoordinateService.new('Denver, CO')
-    expect(coordinate_service).to be_a(CoordinateService)
+    expect(@coordinates_service).to be_a(CoordinateService)
   end
   it 'returns coordinates' do
-    service = CoordinateService.new('10000 E Alameda Avenue,  Denver,  CO')
-    # binding.pry
-
-    expect(service.get_coordinates).to eq({"lat": 39.708285, "lng": -104.8714487})
+    expect(@coordinates_service.get_coordinates).to eq({"lat": 39.7392358, "lng": -104.990251})
   end
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 describe WeatherService do
+  before :each do
+    json_response = File.open("./fixtures/forecast_response.json")
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['dark_sky_key']}/39.708285,-104.8714487")
+    .to_return(status: 200, body: json_response)
+    @service = WeatherService.new({"lat": 39.708285, "lng": -104.8714487})
+
+  end
+
   it 'exists' do
-    service = WeatherService.new({"lat": 39.708285, "lng": -104.8714487})
-    expect(service).to be_a(WeatherService)
+    expect(@service).to be_a(WeatherService)
   end
 
   it 'returns weather' do
-    service = WeatherService.new({"lat": 39.708285, "lng": -104.8714487})
-    expect(service.get_weather[:currently][:summary]).to eq("Mostly Cloudy")
+    expect(@service.get_weather[:currently][:summary]).to eq("Mostly Cloudy")
   end
 end
