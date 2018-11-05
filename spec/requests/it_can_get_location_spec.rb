e0fed20976964b369522f89c82ca55be0f7c2901
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-05 10:52:20 -0700

    Adds webmock to request spec.

--- spec/requests/it_can_get_location_spec.rb
+++ spec/requests/it_can_get_location_spec.rb
@@ -6,6 +6,9 @@ describe 'Get location' do
     expect(response.status).to eq(200)
   end
   it 'returns weather' do
+    json_response = File.open("./fixtures/forecast_response.json")
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['dark_sky_key']}/39.708285,-104.8714487")
+    .to_return(status: 200, body: json_response)
     service = WeatherService.new({"lat": 39.708285, "lng": -104.8714487})
     expect(service.get_weather[:currently][:summary]).to eq("Mostly Cloudy")
   end
