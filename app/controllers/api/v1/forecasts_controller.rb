74355efd966b6619c1f065ae49c3f210eea96a6d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-05 10:51:43 -0700

    removes binding pry

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::ForecastsController < ApplicationController
   def show
     coordinate_service = CoordinateService.new(params[:location])
     weather_service = WeatherService.new(coordinate_service.get_coordinates)
-
-    render json: weather_service.get_weather
+    forecast = ForecastFacade.new(params[:location])
+     render json: weather_service.get_weather
+    # forecast
+    #
   end
 end
