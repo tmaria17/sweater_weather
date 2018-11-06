95e16f081a49abab872c5961c5490172155441ac
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-06 12:01:40 -0700

    Final commit

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::GifsController < ApplicationController
     gifs = forecast.weather[:daily][:data].map do |stuff|
       GifService.new(stuff[:summary]).get_gifs
     end
-      
+
     render json: gifs
     # binding.pry
     #gifs[0][:data][0][:url]
