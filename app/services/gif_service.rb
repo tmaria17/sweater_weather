37808aa570dd391beeda3d78f415ef5938f8191b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-06 10:02:07 -0700

    Creates a gif service.

--- app/services/gif_service.rb
+++ app/services/gif_service.rb
@@ -1,18 +1,23 @@
 class GifService
+  def initialize(search)
+    @search = search
+  end
 
-
+  def get_gifs
+    get_json("/v1/gifs/search?&q=#{@search}&limit=25&offset=0&rating=G&lang=en")
+  end
 
 private
 
 def conn
   Faraday.new(url:"https://api.giphy.com") do |faraday|
-    faraday.params["key"] = ENV['giphy_key']
+    faraday.params["api_key"] = ENV['giphy_key']
     faraday.adapter Faraday.default_adapter
   end
 end
 
 def get_json(url)
-  JSON.parse(conn.get(url).body, symbolize_names: true)[:results]
+  JSON.parse(conn.get(url).body, symbolize_names: true)
 end
 
 end
