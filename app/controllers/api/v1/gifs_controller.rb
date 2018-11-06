2d032d4b896c69df5cf8979dcc4001c7853d1e9a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-06 10:02:54 -0700

    Renders gif service in controller.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::GifsController < ApplicationController
   def index
+    coordinate_service = CoordinateService.new(params[:location])
+    weather_service = WeatherService.new(coordinate_service.get_coordinates)
+    gif_service = GifService.new(weather_service.get_weather[:currently][:summary])
+    render json: gif_service.get_gifs
   end
 
 end
