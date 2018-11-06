c443f0a546ac21550219d1a88cf3029902b5c3d3
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-06 11:59:18 -0700

    Adds way to get url out for future references.

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,25 +1,13 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    # coordinate_service = CoordinateService.new(params[:location])
-    # weather_service = WeatherService.new(coordinate_service.get_coordinates)
-    # binding.pry
-   #  summaries = forecast.weather.map do |data|
-   #   data[:currently][:summary]
-   # end
-    # gif_service = GifService.new(weather_service.get_weather[daily][:summary])
     forecast = ForecastFacade.new(params[:location])
 
     gifs = forecast.weather[:daily][:data].map do |stuff|
       GifService.new(stuff[:summary]).get_gifs
     end
- # ^ this grabs all the daily summaries
-# binding.pry
-    # gifs =  searches.map do |search|
-    #           GifService.new(search).get_gifs
-    #         end
+      
     render json: gifs
     # binding.pry
-    # gif_service.get_gifs
-    # binding.pry
+    #gifs[0][:data][0][:url]
   end
 end
