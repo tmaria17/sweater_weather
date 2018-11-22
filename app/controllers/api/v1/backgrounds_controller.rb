91e8b42a9ed7ad1de82059bcd611c6b92dd019b1
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-21 18:05:49 -0700

    Adds background service to index action.

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BackgroundsController < ApplicationController
 
   def index
+    coordinate_service = CoordinateService.new(params[:location]).get_coordinates
+    background_image = BackgroundImageService.new
+    image = background_image.get_images(coordinate_service[:lat],coordinate_service[:lng])
+    # binding.pry
   end
 
 end
