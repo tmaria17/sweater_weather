632e51c32038d4df2273c52df492222967d7f910
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-11-21 18:06:34 -0700

    Adds a get images method to the background service.

--- app/services/background_image_service.rb
+++ app/services/background_image_service.rb
@@ -1,10 +1,25 @@
 class BackgroundImageService
 
-
+def get_images(lat,lon)
+  get_json("services/rest/?method=flickr.photos.search&lat=#{lat}&lon=#{lon}&tags==parks,museums,landmarks")
+end
 
 private
 
 def conn
+  Faraday.new(url: 'https://api.flickr.com') do |faraday|
+    faraday.params['api_key'] = ENV['flickr_key']
+    faraday.params['format'] = 'json'
+    faraday.params['nojsoncallback'] = '1'
+    faraday.params['privacy_filter'] = 'public'
+    faraday.params['content_type'] = '1'
+    faraday.params['extras'] = 'url_o'
+    faraday.adapter Faraday.default_adapter
+  end
+end
+
+def get_json(url)
+  JSON.parse(conn.get(url).body, symbolize_names: true)
 end
 
 end
